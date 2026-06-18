cec5cc43702518e3ceae3605f58a8ab780232796
tterkimbi <terungwad34@gmail.com>
2026-06-18 18:25:05 +0100

    Exe7

---
+++ exe7.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func RedirectorHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path == "/legacy" {
+		http.Redirect(w, r, "/v2", 301)
+	}
+	if r.URL.Path == "/v2" {
+		fmt.Fprintln(w, "Welcome to version 2")
+		return
+	}
+}
